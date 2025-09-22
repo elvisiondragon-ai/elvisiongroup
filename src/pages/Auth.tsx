02d2ee5eb0e0e6956383d4709af41fcfc8e5e875
eL <elreyzandra@gmail.com>
2025-09-22 20:51:07 +0700

    🚀 22 SEPTEMBER MELUNCUR 🚀

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -575,7 +575,7 @@ export function Auth({ onLogin }: AuthProps) {
                   options={{
                     action: 'forgot-password',
                     theme: 'light',
-                    size: 'invisible',
+                    size: 'normal',
                     retry: 'auto'
                   }}
                 />
@@ -817,7 +817,7 @@ export function Auth({ onLogin }: AuthProps) {
                     options={{
                       action: 'login',
                       theme: 'light',
-                      size: 'invisible',
+                      size: 'normal',
                       retry: 'auto'
                     }}
                   />
@@ -982,7 +982,7 @@ export function Auth({ onLogin }: AuthProps) {
                     options={{
                       action: 'signup',
                       theme: 'light',
-                      size: 'invisible',
+                      size: 'normal',
                       retry: 'auto'
                     }}
                   />
