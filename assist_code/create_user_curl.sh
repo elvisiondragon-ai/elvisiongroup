cffcc17e014babff423c7325f5671ae60cd5a615
eL <elreyzandra@gmail.com>
2025-09-27 01:12:34 +0700

    Revert "FAST BUTTON  🚀"
    This reverts commit 6250f54969b99677e773ba5f2a7aebdd5584d178.

--- assist_code/create_user_curl.sh
+++
@@ -1,9 +0,0 @@
-curl -X POST "https://nlrgdhpmsittuwiiindq.supabase.co/auth/v1/admin/users" \
--H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5scmdkaHBtc2l0dHV3aWlpbmRxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NDQwOTQ1NCwiZXhwIjoyMDY5OTg1NDU0fQ.zA37zRBUtN4Wx64QuE1CTlWiHzphbe6BCRRz-EtWHsE" \
--H "apikey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5scmdkaHBtc2l0dHV3aWlpbmRxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NDQwOTQ1NCwiZXhwIjoyMDY5OTg1NDU0fQ.zA37zRBUtN4Wx64QuE1CTlWiHzphbe6BCRRz-EtWHsE" \
--H "Content-Type: application/json" \
--d '{
-  "email": "mock16@yahoo.com",
-  "password": "password123", 
-  "email_confirm": true
-}'
