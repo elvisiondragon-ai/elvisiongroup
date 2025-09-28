e860d2de029cbda47779bfc45ad1095edcbb303d
eL <elreyzandra@gmail.com>
2025-09-28 15:13:34 +0700

    28 PIXEL Only NO CAPI 🚀

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,6 +24,24 @@ import { cn } from "@/lib/utils";
 import heroImage from "@/assets/hero-meditation.jpg";
 import faviconImage from "@/assets/favicon.png";
 
+declare global {
+  interface Window {
+    fbq: any;
+  }
+}
+
+if (typeof window !== 'undefined' && !window.fbq) {
+  (function(f: any, b: any, e: any, v: any, n?: any, t?: any, s?: any) {
+    if (f.fbq) return; n = f.fbq = function() { n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments); };
+    if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = [];
+    t = b.createElement(e); t.async = !0; t.src = v; s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s);
+  })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+  window.fbq('init', '3319324491540889');
+  window.fbq('track', 'PageView');
+  window.fbq('track', 'ViewContent');
+  window.fbq('trackCustom', 'TestEvent', {}, { test_event_code: 'TEST52099' });
+}
+
 interface HomeProps {
   onNavigate: (tab: string) => void;
 }
