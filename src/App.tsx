3572d27de2fe73028dbb0a89b840b521ab865f4b
eL <elreyzandra@gmail.com>
2025-10-01 17:56:06 +0700

    🔑 1 -6 GLOBAL AUDIO  TOAST  Oktober🚀

--- src/App.tsx
+++ src/App.tsx
@@ -214,6 +214,9 @@ const AppContent = () => {
       // Reset blocker so notification can show for new updates  
       localStorage.removeItem('force-refresh-completed')
 
+      //Hanya 1 OKTOBER ONLY (REMOVE LATER) - Auto update service worker
+      updateServiceWorker(true)
+
     }
   })
 
